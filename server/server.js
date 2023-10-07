34258b1b14879c1d86e9c5fa08c4fc2f4f73cf37
Abdus-samad <abdussamadajao@gmail.com>
2023-10-07 20:28:42 +0100

    done

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const connectDB = require("./src/config/db");
+const path = require("path");
+const app = express();
+require("dotenv").config();
+
+// Connect Database
+connectDB();
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+// Define Routes
+app.use("/api/users", require("./src/routes/users"));
+app.use("/api/auth", require("./src/routes/auth"));
+app.use("/api/contacts", require("./src/routes/contacts"));
+
+// Serve Static assets in production
+if (process.env.NODE_ENV === "production") {
+  //  Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
+
+const PORT = process.env.PORT || 5500;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
